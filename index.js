925f9525f5cc5a57f8c73e7eb473069976095c0e
navanithnithin <132982323+NithinS233@users.noreply.github.com>
2024-07-24 10:18:30 +0530

    basic of node and express 01

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
 const http = require("http");
-const fs = require("fs");
-const url = require("url")
+// const fs = require("fs");
+// const url = require("url");
+const express = require("express");
 
 
-const myServer = http.createServer((req, res) => {
-    if (res.url === "/favicon.ico") return res.end();
-    const log = `${Date.now()}: ${req.method} ${req.url} New request received\n`;
-    const myUrl = url.parse(req.url, true);
 
-    fs.appendFile("log.txt", log, (err, data) => {
-        // res.end("Hello from server");
-        switch (myUrl.pathname) {
-            case "/":
-                if(req.method == 'GET') res.end("Home Page");
-                break;
-            case "/about":
-                const username = myUrl.query.myname;
-                res.end(`hi, ${username}`)
-                break;
-            case "/signup":
-                if (req.method === 'GET') res.end("this is a signup form");
-                break;
-            case "/search":
-                const search = myUrl.query.search_query;
-                res.end("Here are your result for" + search);
-            default:
-                res.end("404 error")
-        }
-    })
+// function myHandler(req, res) {
+//     if (res.url === "/favicon.ico") return res.end();
+//     const log = `${Date.now()}: ${req.method} ${req.url} New request received\n`;
+//     const myUrl = url.parse(req.url, true);
+
+//     fs.appendFile("log.txt", log, (err, data) => {
+//         // res.end("Hello from server");
+//         switch (myUrl.pathname) {
+//             case "/":
+//                 if (req.method == 'GET') res.end("Home Page");
+//                 break;
+//             case "/about":
+//                 const username = myUrl.query.myname;
+//                 res.end(`hi, ${username}`)
+//                 break;
+//             case "/signup":
+//                 if (req.method === 'GET') res.end("this is a signup form");
+//                 break;
+//             case "/search":
+//                 const search = myUrl.query.search_query;
+//                 res.end("Here are your result for" + search);
+//             default:
+//                 res.end("404 error")
+//         }
+//     })
+// }
+
+
+const app = express();
+app.get("/" ,(req, res)=>{
+    return res.send("Hello from home")
 });
+
+app.get("/about", (req, res)=>{
+    return res.send("Hello from about")
+})
+
+
+const myServer = http.createServer(app);
 
 myServer.listen(3000, () => {
     console.log("server stated");
